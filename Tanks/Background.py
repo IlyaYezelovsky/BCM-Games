4eae4ba2634334544740a67fc313cabb83fe8a2e
Ilya Ivanovsky <88927578+IlyaIvanovsky@users.noreply.github.com>
2022-08-12 19:04:56 +0800

    Update Background.py

--- Tanks/Background.py
+++ Tanks/Background.py
@@ -3,18 +3,21 @@ import Kitten
 def when_get_signal(signal):
   if signal == 'V1':
     Kitten.change_shape('V1')
+		# 如果得到广播V1,就切换造型到V1
 
 def when_get_signal(signal):
   if signal == 'V2':
     Kitten.change_shape('V2')
+		# 如果得到广播V2,就切换造型到V2
 
 def when():
   while True:
     if (Kitten.value_of_object('Self', 'style_index') == 2 and Kitten.is_key_pressed(32)):
+			# 32号是空格键
       Kitten.change_shape('坦克大战-背景')
+			# 重新开始，切换到原来背景
       def call_events_of(name_ = '坦克1'):
         Kitten.show()
-# 32号按键是空格键
 
       call_events_of()
       def call_events_of(name_ = '坦克2'):
